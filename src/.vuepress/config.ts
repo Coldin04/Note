40ba42a6a5fc71c271b64d4fec61160be3dc19c9
Coldin04 <me@coldin.top>
2024-01-06 13:43:18 +0800

    更新了统计代码，并增加了描述

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -17,9 +17,7 @@ export default defineUserConfig({
   },
 
   head: [
-    ['script',{charset:'UTF-8',id:'LA_COLLECT',src:'https://sdk.51.la/js-sdk-pro.min.js'}],
-    ['script',{},'LA.init({id:"3H5Sh7lwHvY3yVI3",ck:"3H5Sh7lwHvY3yVI3",autoTrack:true,hashMode:true})'],
-    ['script',{src: 'https://sdk.51.la/perf/js-sdk-perf.min.js',crossorigin:'anonymous'}],
+    ['script',{},'!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3H5Sh7lwHvY3yVI3",ck:"3H5Sh7lwHvY3yVI3"});'],
   ],
 
 
